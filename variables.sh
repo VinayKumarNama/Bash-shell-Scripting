b661f73778e929f5ed82b0548de65e55bfc438b2
VinayKumarNama <namavinay451@gmail.com>
2023-06-03 19:43:52 -0400

    Auto Push

---
+++ variables.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# a=10
+# name=vinay
+# team=AQA
+# topic=Bash Scripting
+
+#in bash there is no concept of Data types, Everything is string
+
+# Declaring Variables
+a=10
+b=20
+c=30
+
+#here $ will print the value of variables
+echo $a
+echo Value of B variable is $b
