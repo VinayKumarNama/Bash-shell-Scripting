ebfa0e2f12c722119fa16be6606a66236a0a1804
Nama <i29728@air-worldwide.com>
2023-06-12 00:09:02 -0400

    Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 Component = ${Component}
+LogFile = "/tmp/${Component}.log"
 ID=$(id -u)
 if [ $ID -ne 0 ] ; then 
     echo -e "\e[31m This script is expected to be run by a root user or with a sudo privilege \e[0m"
@@ -15,7 +16,7 @@ else
 fi
 }
 echo -n "Installing Nginx : "
-yum install nginx -y &>> "/tmp/${Component}.log"
+yum install nginx -y &>> ${LogFile}
 Stat $?
 echo -n "Downloading ${Component} component :"
 curl -s -L -o /tmp/${Component}.zip "https://github.com/stans-robot-project/${Component}/archive/main.zip"
@@ -24,9 +25,10 @@ echo -n "Performing Cleanup :"
 cd /usr/share/nginx/html
 rm -rf * &>> "/tmp/${Component}.log"
 Stat $?
-
-# unzip /tmp/${Component}.zip
- #mv ${Component}-main/* .
- #mv static/* .
- #rm -rf ${Component}-main README.md
- #mv localhost.conf /etc/nginx/default.d/roboshop.conf
+echo -n "Extracting content :"
+unzip /tmp/${Component}.zip &>> ${LogFile}
+mv ${Component}-main/* . &>> ${LogFile}
+mv static/* . &>> ${LogFile}
+rm -rf ${Component}-main README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+Stat $?
