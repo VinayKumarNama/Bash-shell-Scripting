93d09c03ef6fec68cb9d24f4f7bc89fc711aa978
Nama <i29728@air-worldwide.com>
2023-06-11 23:46:25 -0400

    Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+Component = frontend
 ID=$(id -u)
 if [ $ID -ne 0 ] ; then 
     echo -e "\e[31m This script is expected to be run by a root user or with a sudo privilege \e[0m"
@@ -7,7 +7,7 @@ if [ $ID -ne 0 ] ; then
 fi
 
 echo "Installing Nginx : "
-yum install nginx -y
+yum install nginx -y &>> "/tmp/${Component}.log"
 
 # yum install nginx -y
 # systemctl enable nginx
