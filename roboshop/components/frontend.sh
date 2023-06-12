048577b3d20671b72ce8a5281ceb1b0a525dbeb1
Nama <i29728@air-worldwide.com>
2023-06-11 23:29:30 -0400

    Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+Id=$(id -u)
+if[id -ne 0]; then
+    echo -m "\e[31m This script is expected to be run as root user or sudo privilege \e[0m"
+    exit 1
+fi
+
 echo "Installing Nginx : "
 yum install nginx -y
 
