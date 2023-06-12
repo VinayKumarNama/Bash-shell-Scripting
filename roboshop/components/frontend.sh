6c1acfef5bad03023756389a24fd953cca737ed8
Nama <i29728@air-worldwide.com>
2023-06-12 00:25:23 -0400

    Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-Component = frontend
+COMPONENT = frontend
 
 ID=$(id -u)
 if [ $ID -ne 0 ] ; then 
     echo -e "\e[31m This script is expected to be run by a root user or with a sudo privilege \e[0m"
     exit 1
 fi
-LOGFILE="/tmp/${Component}.log"
+LOGFILE="/tmp/${COMPONENT}.log"
 Stat()
 {
 if [ $1 -eq 0 ] ; then
@@ -19,17 +19,17 @@ fi
 echo -n "Installing Nginx : "
 yum install nginx -y   &>> $LOGFILE
 Stat $?
-echo -n "Downloading ${Component} component :"
-curl -s -L -o /tmp/${Component}.zip "https://github.com/stans-robot-project/${Component}/archive/main.zip"
+echo -n "Downloading ${COMPONENT} COMPONENT :"
+curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
 Stat $? 
 echo -n "Performing Cleanup :"
 cd /usr/share/nginx/html
-rm -rf * &>> "/tmp/${Component}.log"
+rm -rf * &>> "/tmp/${COMPONENT}.log"
 Stat $?
 echo -n "Extracting content :"
-unzip /tmp/${Component}.zip &>> $LOGFILE
-mv ${Component}-main/* . &>> $LOGFILE
-mv static/* . &>> $LOGFILE
-rm -rf ${Component}-main README.md
+unzip /tmp/${COMPONENT}.zip &>> $LOGFILE
+mv ${COMPONENT}-main/* . 
+mv static/* . 
+rm -rf ${COMPONENT}-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 Stat $?
