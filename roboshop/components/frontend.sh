152290260b86ce48b54b972ccc92b859cab963b9
Nama <i29728@air-worldwide.com>
2023-06-11 23:36:18 -0400

    Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -2,7 +2,7 @@
 
 Id=$(id -u)
 if [Id -ne 0] ; then
-    echo -m "\e[31m This script is expected to be run as root user or sudo privilege \e[0m"
+    echo -e "\e[31m This script is expected to be run as root user or sudo privilege \e[0m"
     exit 1
 fi
 
