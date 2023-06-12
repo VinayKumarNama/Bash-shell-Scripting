b2c9fa1b1039affffc7cd39a4cfc37a17a647f1f
Nama <i29728@air-worldwide.com>
2023-06-11 23:56:00 -0400

    Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -15,6 +15,14 @@ else
     echo -e "\e[31m Success \e[0m"
 fi
 
+echo -n "Downloading ${Component} component :"
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+if [ $? -eq 0 ] ; then
+    echo -e "\e[32m Success \e[0m"
+else
+    echo -e "\e[31m Success \e[0m"
+fi
+
 # yum install nginx -y
 # systemctl enable nginx
 # systemctl start nginx
