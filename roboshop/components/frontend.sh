7a393c02ef64fdde71835a4c7f080d82aa561266
Nama <i29728@air-worldwide.com>
2023-06-12 00:04:18 -0400

    Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,39 +1,32 @@
 #!/bin/bash
-Component = frontend
+Component = ${Component}
 ID=$(id -u)
 if [ $ID -ne 0 ] ; then 
     echo -e "\e[31m This script is expected to be run by a root user or with a sudo privilege \e[0m"
     exit 1
 fi
-
+Stat()
+{
+if [ $1 -eq 0 ] ; then
+    echo -e "\e[32m Success \e[0m"
+else
+    echo -e "\e[31m Success \e[0m"
+    exit 2
+fi
+}
 echo -n "Installing Nginx : "
 yum install nginx -y &>> "/tmp/${Component}.log"
-
-if [ $? -eq 0 ] ; then
-    echo -e "\e[32m Success \e[0m"
-else
-    echo -e "\e[31m Success \e[0m"
-fi
-
+Stat $?
 echo -n "Downloading ${Component} component :"
-curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
-if [ $? -eq 0 ] ; then
-    echo -e "\e[32m Success \e[0m"
-else
-    echo -e "\e[31m Success \e[0m"
-fi
- 
- echo -n "Performing Cleanup :"
- cd /usr/share/nginx/html
- rm -rf * &>> "/tmp/${Component}.log"
- if [ $? -eq 0 ] ; then
-    echo -e "\e[32m Success \e[0m"
-else
-    echo -e "\e[31m Success \e[0m"
-fi
+curl -s -L -o /tmp/${Component}.zip "https://github.com/stans-robot-project/${Component}/archive/main.zip"
+Stat $? 
+echo -n "Performing Cleanup :"
+cd /usr/share/nginx/html
+rm -rf * &>> "/tmp/${Component}.log"
+Stat $?
 
-# unzip /tmp/frontend.zip
- #mv frontend-main/* .
+# unzip /tmp/${Component}.zip
+ #mv ${Component}-main/* .
  #mv static/* .
- #rm -rf frontend-main README.md
+ #rm -rf ${Component}-main README.md
  #mv localhost.conf /etc/nginx/default.d/roboshop.conf
