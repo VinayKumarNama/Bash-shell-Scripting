f22339c75982ed8a5f9088096b4c2254ea3fe096
Nama <i29728@air-worldwide.com>
2023-06-14 15:15:07 -0400

    Auto Push

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -9,28 +9,28 @@ echo -e "*********** \e[35m $COMPONENT Installation has started \e[0m **********
 echo -n "Configuring the $COMPONENT repo: "
 curl -s https://packagecloud.io/install/repositories/${COMPONENT}/erlang/script.rpm.sh | sudo bash   &>> $LOGFILE
 curl -s https://packagecloud.io/install/repositories/${COMPONENT}/${COMPONENT}-server/script.rpm.sh | sudo bash     &>> $LOGFILE
-stat $? 
+Stat $? 
 
 echo -n "Installing $COMPONENT: "
 yum install ${COMPONENT}-server -y      &>> $LOGFILE 
-stat $? 
+Stat $? 
 
 echo -n "Starting $COMPONENT :"
 systemctl enable ${COMPONENT}-server    &>> $LOGFILE
 systemctl restart ${COMPONENT}-server   &>> $LOGFILE
-stat $?
+Stat $?
 
 # This needs to run only if the user account doesn't exist 
 rabbitmqctl list_users | grep roboshop  &>> $LOGFILE 
 if [ $? -ne 0 ] ; then 
     echo -n "Creating the $COMPONENT $APPUser :"
     rabbitmqctl add_user roboshop roboshop123    &>> $LOGFILE
-    stat $?
+    Stat $?
 fi 
 
 echo -n "Configuring the $COMPONENT $APPUser privileges:"
 rabbitmqctl set_user_tags roboshop administrator     &>> $LOGFILE
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"     &>> $LOGFILE
-stat $? 
+Stat $? 
 
 echo -e "*********** \e[35m $COMPONENT Installation has completed \e[0m ***********"
