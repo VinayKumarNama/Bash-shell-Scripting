60e14bd0d9bacf5b7ea5187c8839079af14ef44b
Nama <i29728@air-worldwide.com>
2023-06-14 15:05:31 -0400

    Auto Push

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -51,7 +51,7 @@ CONFIGURE_SVC()
     echo -n "Updating the $COMPONENT systemd file :"
     sed -i -e 's/AMQPHOST/rabbitmq.roboshop.internal/' -e 's/USERHOST/user.roboshop.internal/' -e 's/CARTHOST/cart.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/${APPUser}/${COMPONENT}/systemd.service  
     mv /home/${APPUser}/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
-    stat $? 
+    Stat $? 
     echo -n "start the ${COMPONENT} Service :"
     systemctl daemon-reload  &>> $LOGFILE
     systemctl enable ${COMPONENT} &>> $LOGFILE
