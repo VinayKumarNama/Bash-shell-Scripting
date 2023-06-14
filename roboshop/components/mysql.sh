c4b1aeb43114dc3d5ebf1467fea1f1028f84b03a
Nama <i29728@air-worldwide.com>
2023-06-14 13:43:33 -0400

    Auto Push

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-echo "I am front end" 
+COMPONENT=mysql
+source components/common.sh
+
+echo -n "Configuring ${COMPONENT} repo :"
+curl -s -L -o /etc/yum.repos.d/${COMPONENT}.repo https://raw.githubusercontent.com/stans-robot-project/${COMPONENT}/main/${COMPONENT}.repo
+Stat $?
+echo -n "Installing ${COMPONENT} :"
+yum install ${COMPONENT}-community-server -y &>> $LOGFILE
+echo -n "starting ${COMPONENT} service :"
+systemctl enable mysqld 
+systemctl start mysqld
+Stat $?
