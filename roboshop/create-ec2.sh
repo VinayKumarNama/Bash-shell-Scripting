e3703c0b67881f925985aa58855ef597a6c35124
Nama <i29728@air-worldwide.com>
2023-06-14 17:02:18 -0400

    Auto Push

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 # AMI_ID="ami-0c1d144c8fdd8d690"
 
-AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId')
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')
 echo "AMI ID is $AMI_ID"
