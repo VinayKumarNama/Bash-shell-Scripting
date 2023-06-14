0b0da83dfe0b2b6ecef924dc09ec3322c74a669c
Nama <i29728@air-worldwide.com>
2023-06-14 15:41:20 -0400

    Auto Push

--- 4-dynamicVar.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-Today_Date=$(date +%F)
-number_of_sessions=$(who | wc -l)
-echo "Good Morning and Today Date is $Today_Date"
-echo "Number of Session opened is : $number_of_sessions"
--- 6-read.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-read -p "Enter your name:" name
-echo -e "Name of the entered Value is $name"
