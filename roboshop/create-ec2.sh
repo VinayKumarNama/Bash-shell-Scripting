5a1bcb48bcc83e6c0cd26e10e69de9fefdc1b7df
Nama <i29728@air-worldwide.com>
2023-06-15 13:50:46 -0400

    Auto Push

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -4,8 +4,10 @@
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')
 SG_ID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=b54-allow-all | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 COMPONENT=$1
+ENV=$2
 echo "AMI ID is $AMI_ID"
 echo -e "AMI ID used to launch the EC2 is \e[35m $AMI_ID \e[0m"
 echo -e "Security Group ID used to launch the EC2 is \e[35m $SG_ID \e[0m"
 echo -e "****** Launching Ec2 Server ********"
-aws ec2 run-instances  --image-id ${AMI_ID}  --instance-type t2.micro   --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g'
+IPADDRESS=$(aws ec2 run-instances  --image-id ${AMI_ID}  --instance-type t2.micro   --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT-$ENV}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+echo -e "Private IP Address of $COMPONENT-$ENV is \e[35m $IPADDRESS \e[0m"
